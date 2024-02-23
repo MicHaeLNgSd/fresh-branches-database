41d02c99263b25657bfde4ff2f556f19e9afca06
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-02-23 19:23:14 +0200

    Add select update delete alter

--- sql/file.sql
+++ sql/file.sql
@@ -1,2 +1,20 @@
 --Valid SQL code
 CREATE DATABASE my_db_name
+
+/*
+  SQL поділяється на наступні "розділи" / під-мови
+  
+    Data Definition Language DDL - мова визначення структури даних 
+      розпочинаються на CREATE, ALTER, DROP, TRUNCATE
+
+    Data Manipulation Lang DML - мова маніпуляції даними
+      INSERT, UPDATE, DELETE
+
+    Data Control Lang DCL - мова контролю даних 
+
+    Transaction Control Lang TCL - мова контролю транзакцій
+      TRANSACTION, COMMIT, ROLLBACK
+
+    Data Query Lang DQL - мова запиту даних
+      тількі одна команда SELECT
+*/
